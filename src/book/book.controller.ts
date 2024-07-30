7df0f7b24a43012d698193d9514e11299d13be7c
lijiahong <1471363285@qq.com>
2024-07-30 23:07:22 +0800

    feat: 图书管理系统服务端完成

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,14 +1,20 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
   Param,
   ParseIntPipe,
   Post,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/cerate-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { storage } from './my-file-storage';
+import * as path from 'path';
 
 @Controller('book')
 export class BookController {
@@ -36,5 +42,31 @@ export class BookController {
   @Post('delete/:id')
   delete(@Param('id', ParseIntPipe) id: number) {
     return this.bookService.deleteId(id);
+  }
+
+  @Post('uoloadFile')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'upload',
+      storage: storage,
+      limits: {
+        fileSize: 3 * 1024 * 1024, // 3MB
+      },
+      fileFilter(req, file, callback) {
+        const extname = path.extname(file.originalname);
+        if (!['.png', '.jpg', '.jpeg', '.gif'].includes(extname)) {
+          callback(
+            new BadRequestException('仅支持.png, .jpg, .jpeg, .gif 后缀图片'),
+            false,
+          );
+        }
+        callback(null, true);
+      },
+    }),
+  )
+  upload(@UploadedFile() file: Express.Multer.File) {
+    console.log('file', file);
+
+    return '/' + file.path.replace('\\', '/');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestApplication>(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({transform: true}));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.useStaticAssets(join(__dirname, '../upload'), { prefix: '/upload' });
 
   await app.listen(3000);
 }
